8fe4225b0df90467f4d357b786fed66234b8eed9
Boran <boran.u3@gmail.com>
2025-05-21 23:00:51 +0200

    init

---
+++ astro.config.mjs
@@ -0,0 +1,41 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: "",
+      logo: {
+        light: "./src/assets/homelab-light.svg",
+        dark: "./src/assets/homelab-dark.svg",
+      },
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/withastro/starlight",
+        },
+      ],
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", slug: "guides/example" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+      customCss: ["./src/styles/global.css"],
+    }),
+  ],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
